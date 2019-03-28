96b978e15bd20a7d26eab73a20efa741c640fdf2
Costa Nonato <costa@nonato.com>
2019-03-27 22:55:24 -0300

    Perguntas service

--- src/app/services/perguntas.service.ts
+++ src/app/services/perguntas.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, DocumentChangeAction } from "angularfire2/firestore";
+import { Observable } from "rxjs/Observable";
+import 'rxjs//add/operator/map';
+import { Pergunta } from "../models";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class PerguntasService {
 
-  constructor() { }
+export class PerguntasService {
+  readonly PERGUNTAS_COLLECTION: string = 'perguntas';
+  private perguntasCollection: AngularFirestoreCollection<Pergunta>;
+
+  constructor(private afs: AngularFirestore) { 
+    this.perguntasCollection = this.afs.collection<Pergunta>(
+      this.PERGUNTAS_COLLECTION
+    );
+  }
+    obterPerguntas(): Observable<Pergunta[]> {
+      return this.perguntasCollection
+      .snapshotChanges()
+      .map(this.mapearIds);
+  }
+  mapearIds(perguntas: DocumentChangeAction<Pergunta>[]): Pergunta[] {
+    return perguntas.map(objPergunta => {
+      const pergunta = objPergunta.payload.doc.data() as Pergunta;
+      pergunta.id = objPergunta.payload.doc.id;
+      return pergunta;
+    });e
+  }
 }
