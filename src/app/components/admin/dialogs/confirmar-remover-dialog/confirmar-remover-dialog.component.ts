6339bffa410eee5e1a8c3d2d5fe8a0c979ee6dcd
Costa Nonato <costa@nonato.com>
2019-03-28 00:15:47 -0300

    html e css confirmar e remover dialog

--- src/app/components/admin/dialogs/confirmar-remover-dialog/confirmar-remover-dialog.component.ts
+++ src/app/components/admin/dialogs/confirmar-remover-dialog/confirmar-remover-dialog.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { 
+	MatDialogRef, MAT_DIALOG_DATA 
+} from '@angular/material'; 
 
 @Component({
   selector: 'app-confirmar-remover-dialog',
   templateUrl: './confirmar-remover-dialog.component.html',
   styleUrls: ['./confirmar-remover-dialog.component.css']
 })
-export class ConfirmarRemoverDialogComponent implements OnInit {
+export class ConfirmarRemoverDialogComponent {
 
-  constructor() { }
+  constructor(
+    private dialogRef: MatDialogRef<ConfirmarRemoverDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) private data: any) { }
 
-  ngOnInit() {
+  enviar() {
+  	this.dialogRef.close({ perguntaId: this.data.perguntaId });
+  }
+
+  fecharDialog() {
+  	this.dialogRef.close();
   }
 
 }
